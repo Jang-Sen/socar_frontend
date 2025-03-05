bc1421e17828526e04d47f38828714d58f25d4ff
jangsen <dh7895210@naver.com>
2025-03-05 15:25:36 +0900

    Add logout

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,18 +1,58 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/authContext";
+import { useQueryClient } from "@tanstack/react-query";
+import { useUserInfo } from "../hook/useUserInfo";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+
+  const { data: user } = useUserInfo();
+
+  const { logout } = useAuth();
+  const queryClient = useQueryClient();
+
+  const logoutHandler = () => {
+    logout();
+    queryClient.removeQueries(["user"]);
+    navigate("/");
+  };
+
   return (
     <div>
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="/">Socar Clone</Navbar.Brand>
+          <Navbar.Brand
+            style={{ cursor: "pointer" }}
+            onClick={() => navigate("/")}
+          >
+            Socar Clone
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link onClick={() => navigate("/car")}>차량 목록</Nav.Link>
+            </Nav>
             <Nav className="ms-auto">
-              <Nav.Link href="/service">이용 안내</Nav.Link>
-              <Nav.Link href="/login">로그인</Nav.Link>
-              <Nav.Link href="/signup">회원가입</Nav.Link>
+              <Nav.Link onClick={() => navigate("/service")}>
+                이용 안내
+              </Nav.Link>
+              {user?.email ? (
+                <>
+                  <Nav.Link onClick={() => navigate("/profile")}>
+                    {user.username}
+                  </Nav.Link>
+                  <Nav.Link onClick={logoutHandler}>로그아웃</Nav.Link>
+                </>
+              ) : (
+                <>
+                  <Nav.Link onClick={() => navigate("/login")}>로그인</Nav.Link>
+                  <Nav.Link onClick={() => navigate("/signup")}>
+                    회원가입
+                  </Nav.Link>
+                </>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
--- src/component/PublicRoute.js
+++ src/component/PublicRoute.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../context/authContext";
 
 const PublicRoute = () => {
   // const accessToken = Cookie.get("accessToken");
   const { user } = useAuth();
+  const location = useLocation(); // 🔥 현재 페이지 확인
 
-  return user ? <Navigate to={"/profile"} replace /> : <Outlet />;
+  if (
+    user &&
+    (location.pathname === "/login" || location.pathname === "/signup")
+  ) {
+    return <Navigate to={"/profile"} replace />;
+  }
+
+  return <Outlet />;
 };
 
 export default PublicRoute;
